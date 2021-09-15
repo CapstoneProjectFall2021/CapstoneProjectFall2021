a3cfa0103f189dd3faa80bfd99cd98721d1a4353
Capstone Project <anhdaica0609@gmail.com>
2021-09-15 18:43:56 +0700

    fix sidebar

--- assets/js/hola-land/hola-land.js
+++ assets/js/hola-land/hola-land.js
@@ -4,16 +4,17 @@
 
 let theme = localStorage.getItem('data-theme');
 const checkbox = document.getElementById("switch");
+
 const changeThemeToDark = () => {
   document.documentElement.setAttribute("data-theme", "dark");
   localStorage.setItem("data-theme", "dark");
-  console.log("I give you dark");
+  checkbox.checked = true;
 }
 
 const changeThemeToLight = () => {
   document.documentElement.setAttribute("data-theme", "light");
   localStorage.setItem("data-theme", 'light');
-  console.log("I give you light");
+  checkbox.checked = false;
 }
 
 if (theme === 'dark') {
